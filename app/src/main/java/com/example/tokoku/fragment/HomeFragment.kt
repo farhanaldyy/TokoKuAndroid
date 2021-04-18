34f7f9ab4d3f2ac08b38868388ef9929092adda0
farhan aldiansyah <farhanpoetra011@gmail.com>
2021-04-19 01:03:11 +0700

    membuat slider dan bottom naigation

---
+++ app/src/main/java/com/example/tokoku/fragment/HomeFragment.kt
@@ -0,0 +1,81 @@
+package com.example.tokoku.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import com.example.tokoku.R
+import com.example.tokoku.adapter.AdapterSlider
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    lateinit var vpSlider: ViewPager
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view : View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        vpSlider = view.findViewById(R.id.vp_slider)
+
+        //array untuk gambar
+        val arraySlider = ArrayList<Int>()
+        arraySlider.add(R.drawable.slide1)
+        arraySlider.add(R.drawable.slide2)
+        arraySlider.add(R.drawable.slide3)
+
+        // panggil adapter
+        val adapterSlider = AdapterSlider(arraySlider, activity)
+        vpSlider.adapter = adapterSlider
+
+
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
