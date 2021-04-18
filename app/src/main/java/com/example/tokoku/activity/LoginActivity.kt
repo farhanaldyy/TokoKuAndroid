cf9581aba92bfc66cc9e6972e3cec199f639ff1a
farhan aldiansyah <farhanpoetra011@gmail.com>
2021-04-19 03:39:28 +0700

    Menambahkan activity login

---
+++ app/src/main/java/com/example/tokoku/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tokoku.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tokoku.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baru)
+    }
+}
